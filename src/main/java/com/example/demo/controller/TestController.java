91ef7af98f7626831672f7c1ba86fc8d3b514af7
Trickjacky <179727527@qq.com>
2019-11-12 17:21:56 +0800

    test1.0

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("测试专用");
+    }
+
+    public void aaa(){
+
+    }
+
+    public void bbb(){
+
+    }
+}
